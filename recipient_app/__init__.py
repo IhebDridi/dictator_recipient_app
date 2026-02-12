62a639584d17a69b6d261aed81d622371b70fe2b
iheb dridi <drdrhbhb5@gmail.com>
2026-02-12 14:35:11 +0100

    last change to rounds number for last recipient

--- recipient_app/__init__.py
+++ recipient_app/__init__.py
@@ -186,7 +186,7 @@ class Results(Page):
         if not self.participant.vars.get("allocations_done", False):
             assign_dictator_rounds_final(
                 recipient_prolific_id=self.participant.label,
-                x=110,
+                x=120,
             )
             self.participant.vars["allocations_done"] = True
 
